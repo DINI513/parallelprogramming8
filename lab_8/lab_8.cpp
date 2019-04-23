c0f85a3ea5aa27b586dc12cbd0e738fd5cb8a6a2
DINI513 <irisha1555@gmail.com>
2019-04-23 10:03:27 +0500

    first commit

--- lab_8/lab_8.cpp
+++ lab_8/lab_8.cpp
@@ -2,20 +2,58 @@
 //
 
 #include "pch.h"
+#include "mpi.h"
 #include <iostream>
+#include <windows.h>
+using namespace std;
 
-int main()
+double f(double);
+
+double f(double a)
 {
-    std::cout << "Hello World!\n"; 
+	return (4.0 / (1.0 + a * a));
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+int main(int argc, char *argv[])
+{
+	int n, myid, numprocs, i;
+	double PI25DT = 3.141592653589793238462643;
+	double mypi, pi, h, sum, x;
+	double startwtime = 0.0, endwtime;
+	int namelen;
+	char processor_name[MPI_MAX_PROCESSOR_NAME];
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+	MPI_Get_processor_name(processor_name, &namelen);
+
+	fprintf(stdout, "Process %d of %d is on %s\n", myid, numprocs, processor_name);
+	fflush(stdout);
+
+	n = 100000000000;  /* default # of rectangles */
+	if (myid == 0)
+		startwtime = MPI_Wtime();
+
+	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	h = 1.0 / (double)n;
+	sum = 0.0;
+	/* A slightly better approach starts from large i and works back */
+	for (i = myid + 1; i <= n; i += numprocs) {
+		x = h * ((double)i - 0.5);
+		sum += f(x);
+	}
+	mypi = h * sum;
+
+	MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+	if (myid == 0) {
+		endwtime = MPI_Wtime();
+		printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
+		printf("wall clock time = %f\n", endwtime - startwtime);
+		fflush(stdout);
+	}
+	MPI_Finalize();
+	return 0;
+}
